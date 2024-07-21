85fd5792b42185b7b1c2ada3dbb784e0a877c603
Vitor Gabriel <vs569119@gmail.com>
2024-07-21 19:28:11 -0300

    Atualizando custom elements

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -7,38 +7,7 @@ class Card extends HTMLElement {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
-    <style>
-      .card {
-        padding: 1rem 0.3rem;
-        border-radius: none;
-        margin-bottom: 1rem;
-        height: 450px;      
-      }
-
-      .card-title, .card-text {
-        margin-bottom: 1rem;
-      }
-
-      .card-title {
-        font-size: 14px;
-        margin-top: 1rem;
-        text-align: justify;      
-      }
-
-      .card-text {
-        font-weight: bold;
-      }
-
-      .btn {
-        padding: 0.5rem 1rem;
-        width: 90%;
-        margin: 0 auto;
-      }
-
-      .card img {
-        height: 200px;
-      }
-    </style>
+    
 
     <img src="${this.getAttribute(
       "img-src"
