dfba810533c91e7fca3a01a4975fe0dd36757ff2
Vitor Gabriel <vs569119@gmail.com>
2024-09-08 19:44:42 -0300

    preço da compra concluído

--- src/js/carrinho/carrinho.js
+++ src/js/carrinho/carrinho.js
@@ -21,6 +21,8 @@ function aumentarQuantidade(quantidadeSpan, preco, total, index) {
 
   atualizarPrecoTotal(quantidadeSpan, preco, total);
   atualizarCarrinho(index, quantidadeAtual);
+
+  calcularValorTotalCarrinho();
 }
 
 // função para diminuir a quantidade
@@ -32,6 +34,8 @@ function diminuirQuantidade(quantidadeSpan, preco, total, index) {
     atualizarPrecoTotal(quantidadeSpan, preco, total);
     atualizarCarrinho(index, quantidadeAtual);
   }
+
+  calcularValorTotalCarrinho();
 }
 
 // função para atualizar o preço com base na quantidade
@@ -73,13 +77,62 @@ function removerProduto(i) {
   localStorage.setItem("carrinho", JSON.stringify(carrinho));
 
   exibirCarrinho();
+
+  calcularValorTotalCarrinho();
 }
 
 // função para calcular o valor total das compras
 function calcularValorTotalCarrinho() {
   const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+  let totalCarrinho = 0;
 
   if (carrinho.length === 0) return;
+
+  carrinho.forEach((produto) => {
+    totalCarrinho += produto.totalPrice
+      ? converterValor({ textContent: produto.totalPrice })
+      : 0;
+  });
+
+  const comprasContainer = document.querySelector("#compras-container");
+  comprasContainer.innerHTML = `
+    <div class="box">
+      <header>
+        <h4>Resultado da compra</h4>
+      </header>
+      <div class="info-compra">
+        <div>
+          <span>Sub-total</span>
+          <span>${formatarComoMoeda(totalCarrinho)}</span>
+        </div>
+        <div>
+          <span>Frete</span>
+          <span>Gratuito</span>
+        </div>
+        <div>
+          <button id="cupom-desconto">Adicionar cupom de desconto</button>
+          <i class="bi bi-arrow-right"></i>
+        </div>
+      </div>
+      <footer id="resultado-compras">
+        <span>Total</span>
+        <span id="valor-final">${formatarComoMoeda(totalCarrinho)}</span>
+      </footer>
+    </div>
+    <button type="submit">Finalizar</button>
+  `;
+
+  const finalizarBtn = document.querySelector("#compras-container");
+
+  finalizarBtn.addEventListener("click", () => {
+    alert(
+      `Compra realizada. O valor total foi de ${formatarComoMoeda(
+        totalCarrinho
+      )}!`
+    );
+
+    window.location.href = "index.html";
+  });
 }
 
 // função para exibir o carrinho
@@ -178,3 +231,5 @@ function exibirCarrinho() {
 // chamando função
 
 exibirCarrinho();
+
+calcularValorTotalCarrinho();
